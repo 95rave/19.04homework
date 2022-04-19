185cfdd29adce06098b05af078dcc0f4c299d63f
95rave <ravanaap@code.edu.az>
2022-04-19 16:11:30 +0400

    task1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _19._04task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( " largest in given" + " array is " + largest());
+        }
+        static int[] arr = { 21, 36, 45, 65, 12, 35 };
+        static int largest()
+        {
+            int i;
+            int max = arr[0];
+
+            for (i = 1; i < arr.Length; i++)
+                if (arr[i] > max)
+                    max = arr[i];
+
+            return max;
+            
+            
+
+            
+        }
+
+    }
+}
